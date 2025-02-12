2715d09d6de066df6f08ffa39f307188563588a3
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2025-02-12 20:53:01 +0100

    adapted to changed sold-out tag and regex more robust

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Browser } from "happy-dom";
+import {Browser} from "happy-dom";
 import ical from "ical-generator";
 import fs from "node:fs";
 
@@ -36,20 +36,28 @@ function parseDate(day, time) {
   );
 }
 
-async function getRuntime(url, browser) {
+async function getRuntime(title, url, browser) {
   const moviePage = browser.newPage();
   await moviePage.goto(url);
   await moviePage.waitUntilComplete();
 
   const runtimeElement = moviePage.mainFrame.document.querySelector(".movie__length").textContent.trim();
-  const runtime = runtimeElement.split(" – ").at(1).match(/^(\d+)\s*min.$/)[1];
-  return runtime;
+  return runtimeElement.split(/\s*[-–—]\s*/).at(1).match(/(\d+)\s*min\.?/)[1];
+}
+
+function isSoldOut(element) {
+  const sold = element.querySelector(".card__status");
+  return !!(sold && sold.textContent.trim().match("Uitverkocht"));
 }
 
 async function scrape(page, browser, callback) {
   const movieElements = page.querySelectorAll(".card--film");
 
   await Promise.all(Array.from(movieElements).map(async (element) => {
+    if (isSoldOut(element)) {
+      return;
+    }
+
     const title = element.querySelector(".card__heading").textContent.trim();
 
     const dayAndTime = (element.querySelector(".card__prose").textContent.trim()).split(" • ");
@@ -59,18 +67,15 @@ async function scrape(page, browser, callback) {
     const start = parseDate(day, time);
 
     const url = element.querySelector(".card__overlay").href;
-
-    const runtime = await getRuntime(url, browser);
+    const runtime = await getRuntime(title, url, browser);
     const end = new Date(start.getTime() + (parseInt(runtime) || 0) * 60_000);
 
     const status = element.querySelector(".card__tag");
     let description;
     if (status) {
-      if (status.textContent.trim().match("Uitverkocht")) return;
-      if (status.textContent.trim().match("Laatste kans!")) description = "Laatste kans";
+      if (status.textContent.trim().match("Laatste kans")) description = "Laatste kans";
       if (status.textContent.trim().match("Eenmalig")) description = "Eenmalig";
     }
-    console.log("STATUS: " + description);
 
     callback({ title, start, end, url, description});
   }))
